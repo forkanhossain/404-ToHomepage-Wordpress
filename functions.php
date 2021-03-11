638f1af8e2cf36d2f71aad25aa5b3b80db9404a6
Md Forkan Hossain <forkanhossain@users.noreply.github.com>
2021-03-11 13:57:57 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+//Add this code in functions.php file
+if( !function_exists('redirect_404_to_homepage') ){
+
+    add_action( 'template_redirect', 'redirect_404_to_homepage' );
+
+    function redirect_404_to_homepage(){
+       if(is_404()):
+            wp_safe_redirect( home_url('/') );
+            exit;
+        endif;
+    }
+}
